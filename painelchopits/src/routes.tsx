e162555faf23760168f402a236817029f995c7f0
CriminalBR <161538911+CriminalBR@users.noreply.github.com>
2025-09-12 21:24:53 -0300

    Add files via upload

---
+++ painelchopits/src/routes.tsx
@@ -0,0 +1,38 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom"
+import { Home } from "./pages/Home"
+import { Categorias } from "./pages/Categorias"
+import { Usuarios } from "./pages/Usuarios"
+import GerenciarUsuarios from "./pages/Usuarios/Gerenciar"
+
+export const Rotas = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Home />}
+                />
+                <Route
+                    path="/categorias/:id"
+                    element={<Categorias />}
+                />
+                <Route
+                    path="/usuarios"
+                    element={<Usuarios />}
+                />
+                <Route
+                    path="/usuarios/:id"
+                    element={<GerenciarUsuarios/>}
+                />
+                <Route
+                    path="*"
+                    element={<h1>404</h1>}
+                />
+            </Routes>
+        </BrowserRouter>
+    )
+}
